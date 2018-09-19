59b1cb602d667314e1008604f60abf9228f68cf0
Matt Zuzolo <mattzuzolo@outlook.com>
2018-09-19 14:19:29 -0400

    refactored /artwork findById and create

--- controllers/artwork_controller.js
+++ controllers/artwork_controller.js
@@ -9,10 +9,39 @@ module.exports = {
   },
 
   findArtworkById(request, response, next){
+    let id = request.params.id;
 
+    if (!ObjectID.isValid(id)){
+      return response.status(404).send();
+    }
+
+    Artwork.findById(id)
+      .then((artwork) => {
+        if(!artwork){
+          return response.status(404).send();
+        }
+        response.send({artwork})
+      })
+      .catch(next);
   },
 
   create(request, response, next){
+    let artwork = new Artwork({
+      title: request.body.title,
+      artist: request.body.artist,
+      medium: request.body.medium,
+      century: request.body.century,
+      culture: request.body.culture,
+      url: request.body.url,
+      primaryimageurl: request.body.primaryimageurl,
+      id: request.body.id,
+    });
 
+    //save to DB or deny entry
+    artwork.save().then((doc) => {
+      response.send(doc);
+    }, (error) => {
+      response.status(400).send(error);
+    });
   },
 };
